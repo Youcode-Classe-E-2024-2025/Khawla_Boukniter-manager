165bb65c1078d1829deb4f7ea776d3781b4e3df4
Khawla Boukniter <boukniter.khawla@gmail.com>
2024-12-22 14:29:35 +0100

    Remove unnecessary vendor and component files

--- vendor/composer/autoload_classmap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$vendorDir = dirname(__DIR__);
-$baseDir = dirname($vendorDir);
-
-return array(
-    'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
-);
--- vendor/composer/autoload_psr4.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$vendorDir = dirname(__DIR__);
-$baseDir = dirname($vendorDir);
-
-return array(
-    'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
-);
